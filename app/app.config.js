51010fb50356bd8978537c93e58757f878e95def
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-09-13 11:04:00 +0200

    Aggiunta della pagina delle condizioni di vendita di Andrea

--- app/app.config.js
+++ app/app.config.js
@@ -1,24 +1,27 @@
 // Principalmente questo file conterrà le route
 angular.module('sitotranquillo')
-.config(['$locationProvider', '$routeProvider',
-    function config($locationProvider, $routeProvider) {
-        // imposto il carattere che mi farà capire che sono in una route
-        //$locationProvider.hashPrefix('!');
+    .config(['$locationProvider', '$routeProvider',
+        function config($locationProvider, $routeProvider) {
+            // imposto il carattere che mi farà capire che sono in una route
+            //$locationProvider.hashPrefix('!');
 
-        // aggiungo le route
-        $routeProvider
-        .when('/', {
-            template: '<home></home>'
-        })
-        .when('/invalidPage', {
-            template: '<invalid></invalid>'
-        })
-        .when('/prodotti/:id', {
-            template: '<dettaglio></dettaglio>'
-        })
-        .otherwise('/invalidPage');
+            // aggiungo le route
+            $routeProvider
+                .when('/', {
+                    template: '<home></home>'
+                })
+                .when('/invalidPage', {
+                    template: '<invalid></invalid>'
+                })
+                .when('/prodotti/:id', {
+                    template: '<dettaglio></dettaglio>'
+                })
+                .when('/condizioni', {
+                    template: '<condizioni></condizioni>'
+                })
+                .otherwise('/invalidPage');
 
-        // tolgo l'hashbang
-        $locationProvider.html5Mode(true);
-    }
-]);
+            // tolgo l'hashbang
+            $locationProvider.html5Mode(true);
+        }
+    ]);
