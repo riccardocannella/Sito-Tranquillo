42af94f8a04a33e017a3d56ec302b67d1ed7ea53
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-09-14 12:54:48 +0200

    Corretto l'issue #26
    Dettagli : https://github.com/ecommerce-unicam/Sito-Tranquillo/issues/26

--- server/API/apiUtenti.js
+++ server/API/apiUtenti.js
@@ -32,44 +32,63 @@ exports.registraUtente = function(req,res) {
                 allora restituisco un errore 409 'Conflict'
             */
             res.status(409).json({'errore': "Esiste già un utente con questo username",
-            'username': req.body.username});
+                                  'username': req.body.username
+                                 });
 
-        } else { // Utente non trovato quindi lo creo
-            
-            var password_hash;
-            var risposta_segreta_hash;
-    
-            bcrypt.hash(req.body.password, encryption.saltrounds)
-            .then(function(pass_hash){ // Creo l'hash della password
-                password_hash = pass_hash;
-                bcrypt.hash(req.body.risposta_segreta, encryption.saltrounds)
-                .then(function(risp_hash){ // Creo l'hash della risposta
-                    risposta_segreta_hash = risp_hash;
+        } else { // Utente non trovato quindi controllo l'email
+            Utente.findOne({email:req.body.email})
+            .then(function(user){
+                if(user){
+                /* 
+                Esiste un utente con la stessa e-mail
+                allora restituisco un errore 409 'Conflict'
+                */
+                res.status(409).json({'errore': "Esiste già un utente con questa e-mail",
+                                      'email': req.body.email
+                                     });
+                } else { // Email disponibile
                     
-                    // creo un utente in base al modello
-                    var nuovoUtente = new Utente({
-                        username: req.body.username,
-                        email: req.body.email,
-                        password_hash: password_hash,
-                        domanda_segreta:req.body.domanda_segreta,
-                        risposta_segreta_hash: risposta_segreta_hash 
+                    // Creo quindi l'utente
+                    var password_hash;
+                    var risposta_segreta_hash;
+            
+                    bcrypt.hash(req.body.password, encryption.saltrounds)
+                    .then(function(pass_hash){ // Creo l'hash della password
+                        password_hash = pass_hash;
+                        bcrypt.hash(req.body.risposta_segreta, encryption.saltrounds)
+                        .then(function(risp_hash){ // Creo l'hash della risposta
+                            risposta_segreta_hash = risp_hash;
+                            
+                            // creo un utente in base al modello
+                            var nuovoUtente = new Utente({
+                                username: req.body.username,
+                                email: req.body.email,
+                                password_hash: password_hash,
+                                domanda_segreta:req.body.domanda_segreta,
+                                risposta_segreta_hash: risposta_segreta_hash 
+                            });
+            
+                            // salvo l'utente nel database
+                            nuovoUtente.save(function(err){
+                                if(err)
+                                    return handleError(res, err, "I valori non hanno superato la validazione del server"); 
+                                else{
+                                    res.status(201).json(nuovoUtente);
+                                }
+                            });
+                        })
+                        .catch(function(err){
+                            return handleError(res,err,"Errore riscontrato durante l'hashing della risposta segreta");
+                        });
+                    }).catch(function(err){
+                        return handleError(res,err,"Errore riscontrato durante l'hashing della password dell'utente");
                     });
-    
-                    // salvo l'utente nel database
-                    nuovoUtente.save(function(err){
-                        if(err)
-                            return handleError(res, err, "Errore durante il salvataggio dell'utente nel database"); 
-                        else{
-                            res.status(201).json(nuovoUtente);
-                        }
-                    });
-                })
-                .catch(function(err){
-                    return handleError(res,err,"Errore riscontrato durante l'hashing della risposta segreta");
-                });
-            }).catch(function(err){
-                return handleError(res,err,"Errore riscontrato durante l'hashing della password dell'utente");
-            });
+                }
+            })
+            .catch(function(err){ // Connessione al database non riuscita
+                return handleError(res,err,'Errore riscontrato durante la connessione al database');
+            })
+            
 
             
         }
