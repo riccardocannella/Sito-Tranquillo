fd362a48cebc129645538e547c58bd4b814c599f
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-09-21 12:43:48 +0200

    implementata la funzionalità, da finire con l'upload delle immagini

--- app/layout/backend/aggiuntaProdotti/aggiuntaProdotti.component.js
+++ app/layout/backend/aggiuntaProdotti/aggiuntaProdotti.component.js
@@ -1,7 +1,14 @@
 // Registra il componente 'aggiuntaProdotti' sul modulo 'aggiuntaProdotti'
 angular.module('aggiuntaProdotti').component('aggiuntaProdotti', {
     templateUrl: 'layout/backend/aggiuntaProdotti/aggiuntaProdotti.template.html',
-    controller: function() {
-        console.log('pippo');
+    controller: function($http, $location) {
+        var aggiuntaProdotto = this;
+        aggiuntaProdotto.aggiungiProdotto = function() {
+            $http.post('api/v1.0/prodotti', aggiuntaProdotto.prodotto).then(
+                function(res) {
+                    $location.path('');
+                }
+            )
+        };
     }
 });
