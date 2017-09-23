6d745350c06285f35f10721359f827c137da994e
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-09-23 21:00:10 +0200

    miglioramenti e eliminazione foto di prova

--- app/layout/backend/aggiuntaProdotti/aggiuntaProdotti.component.js
+++ app/layout/backend/aggiuntaProdotti/aggiuntaProdotti.component.js
@@ -3,28 +3,30 @@ angular.module('aggiuntaProdotti').component('aggiuntaProdotti', {
     templateUrl: 'layout/backend/aggiuntaProdotti/aggiuntaProdotti.template.html',
     controller: function(Upload, $http, $location) {
         var aggiuntaProdotto = this;
-
         aggiuntaProdotto.aggiungiProdotto = function() {
             // validazione immagine da fare (come?)
-            Upload.upload({
-                url: '/api/v1.0/upload',
-                data: { file: aggiuntaProdotto.immagine }
-            }).then(function(resp) {
-                if (resp.data.error_code === 0) {
-                    // valida
-                    aggiuntaProdotto.prodotto.urlImmagine = resp.data.nomeFile;
-                    $http.post('api/v1.0/prodotti', aggiuntaProdotto.prodotto).then(
-                        function(res) {
-                            $location.path('');
-                        }
-                    )
-                } else {
-                    // errore
-                    console.log('errore nell\'invio dell\'immagine');
-                }
-            }, function(resp) {
-                console.log('Error status: ' + resp.status);
-            });
+            // intanto controllo se c'è
+            if (aggiuntaProdotto.immagine) {
+                Upload.upload({
+                    url: '/api/v1.0/upload',
+                    data: { file: aggiuntaProdotto.immagine }
+                }).then(function(resp) {
+                    if (resp.data.error_code === 0) {
+                        // valida
+                        aggiuntaProdotto.prodotto.urlImmagine = resp.data.nomeFile;
+                        $http.post('api/v1.0/prodotti', aggiuntaProdotto.prodotto).then(
+                            function(res) {
+                                $location.path('');
+                            }
+                        )
+                    } else {
+                        // errore
+                        console.log('errore nell\'invio dell\'immagine');
+                    }
+                }, function(resp) {
+                    console.log('Error status: ' + resp.status);
+                });
+            }
         };
     }
 });
--- app/layout/backend/modificaProdotti/modificaProdotti.component.js
+++ app/layout/backend/modificaProdotti/modificaProdotti.component.js
@@ -1,7 +1,7 @@
 // Registra il componente 'modificaProdotti' sul modulo 'modificaProdotti'
 angular.module('modificaProdotti').component('modificaProdotti', {
     templateUrl: 'layout/backend/modificaProdotti/modificaProdotti.template.html',
-    controller: function($http, $location, $routeParams) {
+    controller: function(Upload, $http, $location, $routeParams) {
         var modificaProdotti = this;
         var id = $routeParams.id;
         var prodottoOrig;
@@ -14,14 +14,26 @@ angular.module('modificaProdotti').component('modificaProdotti', {
             modificaProdotti.prodotto = angular.copy(prodottoOrig);
         }
         modificaProdotti.aggiornaProdotto = function() {
-            $http.put('api/v1.0/prodotti/' + id, modificaProdotti.prodotto).then(
-                function(res) {
-                    $location.path('');
-                },
-                function(err) {
-                    console.log('errore!\n', err.data);
-                }
-            )
-        };
+            if (modificaProdotti.immagine) {
+                Upload.upload({
+                    url: '/api/v1.0/upload',
+                    data: { file: modificaProdotti.immagine }
+                }).then(function(resp) {
+                    if (resp.data.error_code === 0) {
+                        // valida
+                        // magari dovrei fare in modo che la vecchia immagine venga cancellata?
+                        modificaProdotti.prodotto.urlImmagine = resp.data.nomeFile;
+                        $http.put('api/v1.0/prodotti/' + id, modificaProdotti.prodotto).then(
+                            function(res) {
+                                $location.path('');
+                            },
+                            function(err) {
+                                console.log('errore!\n', err.data);
+                            }
+                        )
+                    }
+                });
+            }
+        }
     }
 });
