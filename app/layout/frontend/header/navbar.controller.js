bf24502c7895e10277b1923cfcda5e1a02dc67a6
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-10-21 18:22:50 +0200

    fix navbar
    un po' barbaro ma funziona

--- app/layout/frontend/header/navbar.controller.js
+++ app/layout/frontend/header/navbar.controller.js
@@ -5,6 +5,14 @@ var navbar = angular.module('navbar', [
 
 navbar.controller('navbarCtrl', ['$scope', '$location', '$window', function($scope, $location, $window) {
     $scope.nome_utente = "";
+    // Questo blocco l'ho ripetuto qui così ogni volta che l'oggetto "navbarCtrl" viene creato
+    // farà il check, che poi verrà fatto anche ogni volta che si cambia la location
+    if ($window.localStorage.getItem("jwtToken") == "" || $window.localStorage.getItem("jwtToken") == undefined) {
+        $scope.navbarTemplate = 'layout/frontend/header/navbar.noautenticazione.template.html';
+    } else {
+        $scope.navbarTemplate = 'layout/frontend/header/navbar.siautenticazione.template.html';
+        $scope.nome_utente = $window.localStorage.getItem("username");
+    }
     $scope.$on('$locationChangeSuccess', function() { // INVECE DI LOCATIONCHANGE?
         if ($window.localStorage.getItem("jwtToken") == "" || $window.localStorage.getItem("jwtToken") == undefined) {
             $scope.navbarTemplate = 'layout/frontend/header/navbar.noautenticazione.template.html';
