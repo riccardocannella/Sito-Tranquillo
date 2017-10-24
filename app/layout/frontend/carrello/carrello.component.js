2f27a759ebf69639c0cc4c9895856e44e2526e4d
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-10-24 16:37:51 +0200

    Implementati tasti + e - carrello

--- app/layout/frontend/carrello/carrello.component.js
+++ app/layout/frontend/carrello/carrello.component.js
@@ -9,32 +9,76 @@ angular.module('carrello').component('carrello', {
     controller: function($http, $stateParams, $window, $scope) {
         var carrelloCtrl = this;
 
-        console.log('Lisciami le mele');
-        $http({
-            method: 'POST',
-            url: '/api/v1.0/utenti/getCarrello',
-            data: { 'token': $window.localStorage.getItem("jwtToken") }
-        }).then(function successCallback(response) {
-            carrelloCtrl.carrello = response.data.carrello;
-            carrelloCtrl.totaleImporto = 0;
-            
-            // Calcolo del totale 
-            carrelloCtrl.carrello.forEach(function(elemento) {
-                carrelloCtrl.totaleImporto += elemento.totale;
-            }, this);
+        carrelloCtrl.getCarrello = function(){
+            console.log('Lisciami le mele');
+            $http({
+                method: 'POST',
+                url: '/api/v1.0/utenti/getCarrello',
+                data: { 'token': $window.localStorage.getItem("jwtToken") }
+            }).then(function successCallback(response) {
+                carrelloCtrl.carrello = response.data.carrello;
+                carrelloCtrl.totaleImporto = 0;
+                
+                // Calcolo del totale 
+                carrelloCtrl.carrello.forEach(function(elemento) {
+                    carrelloCtrl.totaleImporto += elemento.totale;
+                }, this);
+    
+                carrelloCtrl.tasse = carrelloCtrl.totaleImporto * 0.22;
+                carrelloCtrl.spedizione = 15;
+                carrelloCtrl.totaleFinale = carrelloCtrl.totaleImporto + carrelloCtrl.tasse + carrelloCtrl.spedizione;
+                console.log(JSON.stringify(carrelloCtrl.carrello));
+            }).catch(function errorCallback(response){
+                console.log('Errore: ' + response);
+            });
+        };
+        carrelloCtrl.aggiungiAlCarrello = function(idprodotto){
+            $http({
+                method: 'POST',
+                url: '/api/v1.0/utenti/aggiungialcarrello',
+                data: { 'token': $window.localStorage.getItem("jwtToken"), 'prodotto':idprodotto,'quantita':1 }
+            }).then(function successCallback(response){
+                if(response.data.successo == true){
+                    // Aggiorno il carrello
+                    carrelloCtrl.getCarrello();
+                } else {
+                    alert('impossibile aggiungere altro prodotto');
+                    // Aggiorno il carrello
+                    carrelloCtrl.getCarrello();
+                }
+                
+            }).catch(function errorCallback(response){
+                console.log('not ok');
+            });
+        };
 
-            carrelloCtrl.tasse = carrelloCtrl.totaleImporto * 0.22;
-            carrelloCtrl.spedizione = 15;
-            carrelloCtrl.totaleFinale = carrelloCtrl.totaleImporto + carrelloCtrl.tasse + carrelloCtrl.spedizione;
-            console.log(JSON.stringify(carrelloCtrl.carrello));
-        }).catch(function errorCallback(response){
-            console.log('Errore: ' + response);
-        });
-        
-        // Funzione per la visualizzazione dell'alert dopo aver cliccato sul pulsante Acquista
-        $scope.acquisto = 'Il tuo ordine è stato inviato correttamente!!';
-        $scope.alertAcquistoEffettuato = function(acquisto) {
-            $window.alert(acquisto);
-          };
+        carrelloCtrl.rimuoviDalCarrello = function(idprodotto,quantitaPresente){
+            console.log(quantitaPresente);
+            if(quantitaPresente == 1){
+                alert('Impossibile scendere sotto ad 1 di quantità, per rimuovere il prodotto selezionare il tasto apposito');
+                return;
+            }
+            
+            $http({
+                method: 'POST',
+                url: '/api/v1.0/utenti/rimuovidalcarrello',
+                data: { 'token': $window.localStorage.getItem("jwtToken"), 'prodotto':idprodotto,'quantita':1 }
+            }).then(function successCallback(response){
+                if(response.data.successo == true){
+                    // Aggiorno il carrello
+                    carrelloCtrl.getCarrello();
+                } else {
+                    alert("non è stato possibile rimuovere il prodotto, ci scusiamo per il disagio");
+                    // Aggiorno il carrello
+                    carrelloCtrl.getCarrello();
+                }
+            }).catch(function errorCallback(response){
+                console.log('not ok');
+            });
+        };
+
+        // Eseguo al caricamento della pagina
+        carrelloCtrl.getCarrello();
+
     }
 });
