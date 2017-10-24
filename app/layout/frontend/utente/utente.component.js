a9058ae75befa9587691d9af4f2445a2bd4a77b5
riccardocannella <riccardo.cannella@studenti.unicam.it>
2017-10-24 12:22:18 +0200

    Refactor

--- app/layout/frontend/utente/utente.component.js
+++ app/layout/frontend/utente/utente.component.js
@@ -11,21 +11,21 @@ angular.module('utente').component('utente', {
         var ctrl = this;
         var id = $stateParams.id;
         var utenteOrig;
-        $http.post('api/v1.0/utenti/get', { token: $window.localStorage.getItem("jwtToken") }).then(function(response) {
+        if ($window.localStorage.getItem("jwtToken") == undefined || $window.localStorage.getItem("jwtToken") == '')
+            $location.path('/autenticazione');
+        else {
+            $http.post('api/v1.0/utenti/get', { token: $window.localStorage.getItem("jwtToken") }).then(function(response) {
                 ctrl.utente = response.data;
-                console.log(ctrl.utente);
                 delete ctrl.utente._id;
                 utenteOrig = angular.copy(ctrl.utente);
-            },
-            function(err) {
-                $location.path('/autenticazione');
             });
+        }
         ctrl.reset = function() {
             ctrl.utente = angular.copy(utenteOrig);
         };
         ctrl.aggiornaUtente = function() {
             ctrl.utente.token = $window.localStorage.getItem("jwtToken");
-            $http.put('api/v1.0/utenti/aggiorna/' + id, ctrl.utente).then(
+            $http.put('api/v1.0/utenti/aggiorna', ctrl.utente).then(
                 function(res) {
                     $location.path('');
                 },
@@ -33,6 +33,6 @@ angular.module('utente').component('utente', {
                     console.log('errore!\n', err.data);
                 }
             )
-        };
+        }
     }
 });
