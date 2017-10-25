e36eea669e02782e8c9c2c6dc49da4db1e70fb71
Michele Biondi <michele.biondi@studenti.unicam.it>
2017-10-25 22:40:59 +0200

    Creati i file per la route storiaAcquisti

---
+++ app/layout/frontend/storiaAcquisti/storiaAcquisti.component.js
@@ -0,0 +1,12 @@
+// Definizione del modulo 'informativa'
+angular.module('storiaAcquisti', [
+    'ui.router'
+]);
+
+angular.module('storiaAcquisti').component('storiaAcquisti', {
+    templateUrl: 'layout/frontend/storiaAcquisti/storiaAcquisti.template.html',
+    controller: function($http, $stateParams, $scope){
+        var storiaAcquisti = this;
+        
+    }
+});
